9c4035632aef8017b5b3873a55d099f77868edfd
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2023-05-24 11:32:03 +0000

    Enable debug for blank-canvas in codeworld-api

--- codeworld-api/src/CodeWorld/Driver.hs
+++ codeworld-api/src/CodeWorld/Driver.hs
@@ -904,6 +904,7 @@ runBlankCanvas act = do
             { Canvas.events =
                   ["mousedown", "mouseup", "mousemove", "keydown", "keyup"]
             }
+            True
     putStrLn $ printf "Open me on http://127.0.0.1:%d/" (Canvas.port options)
     Canvas.blankCanvas options $ \context -> do
         putStrLn "Program is starting..."
